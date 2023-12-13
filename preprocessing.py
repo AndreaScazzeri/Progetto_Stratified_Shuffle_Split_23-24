1717cf1afa589fea80fe64421d7680a650467dfd
Andrea Scazzeri <andrea.scazzeri@alcampus.it>
2023-12-13 10:20:33 +0100

    Sono state definite le funzioni in preprocessing.py ed è stato inserito il file che contiene i dati nella cartella del progetto

---
+++ preprocessing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('breast_cancer.csv')
+
+def gestisci_elementi_vuoti(df):
+    pass
+
+def feature_scaling(df):
+    pass
+
+def divisione_features(df):
+    pass
