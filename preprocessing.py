ef52a48a255edb6e3760b55d7c9708f6b1782376
EdoardoABC <giacomettoedoardo@gmail.com>
2023-12-18 11:06:45 +0100

    È stato implementato il metodo di feature scaling

--- preprocessing.py
+++ preprocessing.py
@@ -4,9 +4,19 @@ class PreProcessing():
     def gestisci_elementi_vuoti(df):
         pass
 
-    def feature_scaling(df):
-        pass
+    def feature_scaling(df: pd.DataFrame, tipo='stand'):
 
+        if tipo == 'stand':
+            # Si effettua il feature scaling utilizzando la tecnica di standardizzazione
+            media = np.mean(df, axis=0)
+            deviazione_standard = np.std(df, axis=0)
+            standardizzazione = ((df - media) / deviazione_standard)
+            return media, deviazione_standard, standardizzazione
+        else:
+            df_min = df.min()
+            df_max = df.max()
+            normalizzazione = (df - df_min) / (df_max - df_min)
+            return normalizzazione
     def divisione_features(df:pd.DataFrame):
         dimensioni = df.shape
         var_ind = df.iloc[:, :dimensioni[1] - 1]
