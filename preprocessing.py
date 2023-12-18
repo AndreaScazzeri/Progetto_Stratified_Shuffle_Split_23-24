772c6f710b3a7053d78bf7b273ce09c70007c2a7
Mattia Muraro <mattiamuraro8@gmail.com>
2023-12-18 10:46:20 +0100

    è stata aggiunta la classe pre processing

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-def gestisci_elementi_vuoti(df):
-    pass
+class PreProcessing():
+    def gestisci_elementi_vuoti(df):
+        pass
 
-def feature_scaling(df):
-    pass
+    def feature_scaling(df):
+        pass
 
-def divisione_features(df:pd.DataFrame):
-    dimensioni = df.shape
-    var_ind = df.iloc[:, :dimensioni[1] - 1]
-    prev = df.iloc[:, dimensioni[1] - 1:]
-    return var_ind, prev
+    def divisione_features(df:pd.DataFrame):
+        dimensioni = df.shape
+        var_ind = df.iloc[:, :dimensioni[1] - 1]
+        prev = df.iloc[:, dimensioni[1] - 1:]
+        return var_ind, prev
