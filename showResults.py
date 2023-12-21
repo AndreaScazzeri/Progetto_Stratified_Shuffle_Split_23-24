ea752066b5d2172e1078808e9fcb118820c24c27
Mattia Muraro <mattiamuraro8@gmail.com>
2023-12-21 20:25:17 +0100

    È stato aggiunto il file showResults.py necessario per graficare i risultati ottenuti

---
+++ showResults.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+class ShowResults:
+    '''
+    Classe per mostrare i risultati mediante grafici
+    per rendere la classe più dinamica si può scegliere di implementare diverse funzioni ognuna delle quali fa grafici diversi
+    '''
+    def doBoxplot(self, **metriche):
+        pass
+    def doPlot(self, **metriche):
+        pass
