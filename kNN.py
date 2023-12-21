3d4d5e6d0eebf01eb11e5f94fee2f4b8bef87e61
Mattia Muraro <mattiamuraro8@gmail.com>
2023-12-21 18:29:42 +0100

    È stata aggiunta la classe kNN per implementare l'addestramento

---
+++ kNN.py
@@ -0,0 +1,10 @@
+class KNN:
+    '''
+    Classe KNN che deve implementare l'addestramento del k-Nearest Neighbor con tutte le funzioni necessarie
+    ATTENZIONE: non ci sono particolari pattern da utilizzare per questa classe, quindi chi la implementerà sceglie le funzioni
+    da definire e come farlo
+    '''
+    def doTrain(self):
+        pass
+    def doPrediction(self):
+        pass
