d2f6b73a3212ad57ee69677486fc6757aecd0cf8
Edoardo Giacometto <giacomettoedoardo@gmail.com>
2023-12-24 12:32:10 +0100

    è stato corretto il main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from kNNPipeline import KNNPipeline
 if __name__ == '__main__':
-    pipeline = KNNPipeline('./Progetto_Stratified_Shuffle_Split_23-24/breast_cancer_test.csv')
+    pipeline = KNNPipeline('breast_cancer_test.csv')
     print('Progetto Stratified Shuffle')
