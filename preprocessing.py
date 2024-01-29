c506951a2ed791f4435592aa3b748913800c863e
Mattia Muraro <mattiamuraro8@gmail.com>
2024-01-29 17:49:43 +0100

    Sono stati migliorati i commenti

--- preprocessing.py
+++ preprocessing.py
@@ -1,72 +1,77 @@
 import pandas as pd
 import numpy as np
 from abc import ABC, abstractmethod
-#from featureScalingStand import FeatureScalingStand
-#from featureScalingNorm import FeatureScalingNorm
+
 
 class PreProcessing:
-
-    def gestisci_elementi_vuoti(self,df:pd.DataFrame):
+    def gestisci_elementi_vuoti(self, df: pd.DataFrame):
+        """
+        Elimina le righe che contengono valori nulli
+        """
         df = df.dropna(axis=0)
         new_file = 'new_file'
         df.to_csv(new_file, index=False)
         return df
 
-    def divisione_features(self,df:pd.DataFrame):
-        '''Dato un dataframe restituisce una tupla dove il primo elemento è un dataFrame che contiene tutte le variabili
+    def divisione_features(self, df: pd.DataFrame):
+        """
+        Dato un dataframe restituisce una tupla dove il primo elemento è un dataFrame che contiene tutte le variabili
         indipendenti, mentre il secondo elemento sono le predizioni
-        '''
+        """
         dimensioni = df.shape
         var_ind = df.iloc[:, :dimensioni[1] - 1]
         prev = df.iloc[:, dimensioni[1] - 1:]
         return var_ind, prev
-    def doDataPreprocessing(self,df:pd.DataFrame):
-        '''
+    def doDataPreprocessing(self, df: pd.DataFrame):
+        """
         Funzione della classe PreProcessing che si occupa di effettuare tutto il processo
         di preprocessing dei dati
         :param df:
         :return:
-        '''
+        """
         pass
 
 
-#la classe feature scaling è una classe astratta (strategy pattern)
+# la classe feature scaling è una classe astratta (strategy pattern)
 class FeatureScaling(ABC):
-    '''classe per l'implementazione del pattern strategy per rendere modulare la tipologia di feature
+    """
+    Classe per l'implementazione del pattern strategy per rendere modulare la tipologia di feature
     scale che l'utente vuole scegliere
-    '''
+    """
     @abstractmethod
     def scale(self,df:pd.DataFrame):
         pass
-    #metodo statico della classe astratta per la creazione di oggetti che effettuano lo scale
+    # metodo statico della classe astratta per la creazione di oggetti che effettuano lo scale
     @staticmethod
-    def create(tipo:str='stand'):
+    def create(tipo: str = 'stand'):
         if tipo == 'stand':
             return FeatureScalingStand()
         elif tipo == 'norm':
             return FeatureScalingNorm()
 
-#classe per l'implementazione dello strategy pattern usato per il feature scaling
+# classe per l'implementazione dello strategy pattern usato per il feature scaling
 class FeatureScalingStand(FeatureScaling):
     def scale(self,df:pd.DataFrame):
-        '''Si effettua il feature scaling utilizzando la tecnica di standardizzazione
+        """
+        Si effettua il feature scaling utilizzando la tecnica di standardizzazione
         :param df: dataFrame del quale si vuole effettuare il la standardizzazione
         :return: media = è la media di ogni series presente nel dataFrame
         deviazione_standard = è la deviazione standard di ogni series presente nel DataFrame
         standardizzazione = è lo stesso dataFrame di partenza sul quale è stata effettuato il feature scaling
-        '''
+        """
         media = np.mean(df, axis=0)
         deviazione_standard = np.std(df, axis=0)
         standardizzazione = ((df - media) / deviazione_standard)
         return standardizzazione
 
-#classe per l'implementazione dello strategy pattern usato per il feature scaling
+# classe per l'implementazione dello strategy pattern usato per il feature scaling
 class FeatureScalingNorm(FeatureScaling):
     def scale(self, df: pd.DataFrame):
-        '''Si effettua il feature scaling utilizzando la tecnica di normalizzazione
+        """
+        Si effettua il feature scaling utilizzando la tecnica di normalizzazione
         :param df: dataFrame del quale si vuole effettuare il la normalizzazione
         :return: normalizzazione = è lo stesso dataFrame di partenza sul quale è stata effettuato il feature scaling
-        '''
+        """
         df_min = df.min()
         df_max = df.max()
         normalizzazione = (df - df_min) / (df_max - df_min)
--- stratifiedShuffleSubsamplingSplitter.py
+++ stratifiedShuffleSubsamplingSplitter.py
@@ -1,46 +1,46 @@
 import numpy as np
-
 from splitting import Splitting
 import pandas as pd
 import random
+
+
 class StratifiedShuffleSubsamplingSplitter(Splitting):
     def split(self, df: pd.DataFrame, parametro_splitting, n_divisioni, seed=1):
-        '''
-        Metodo ereditato dalla classe splitting. Serve per splittare con lo stratified shuffle subsampling il dataframe che gli viene passato
+        """
+        Metodo ereditato dalla classe splitting. Serve per splittare con lo stratified shuffle subsampling il dataframe
+        che gli viene passato
         :param df: dataframe da splittare
+        :param parametro_splitting: parametro che indica la percentuale di elementi che devono essere nel test set
         :param n_divisioni: numero di volte che deve essere ripetuto lo splitting
-        :return: deve restituire n_divisioni di coppie train set - test set
-        '''
+        :param seed: parametro che serve a generare sempre gli stessi numeri casuali, utile per la riproducibilitá
+        :return: deve restituire una lista lunga n_divisioni dove ogni elemento è una tupla (train set, test set)
+        """
         if seed is not None:
             random.seed(seed)
-        risultato=[]
-        for i in range(0,n_divisioni):
-            #divido il dataframe in funzione della classe
-            df_classe2=df[df['Class']==2]
-            df_classe4=df[df['Class']==4]
-            #calcolo quale è la numerosita' del test set attesa di ogni classe
-            numerosita_attesa_classe2=int(round(parametro_splitting*len(df_classe2)))
-            numerosita_attesa_classe4=int(round(parametro_splitting*len(df_classe4)))
-            #estraggo gli indici delle righe che devono essere nel test set di ogni classe
-            indici_classe_2=random.sample(range(0,len(df_classe2)),numerosita_attesa_classe2)
+        risultato = []
+        for i in range(0, n_divisioni):
+            # divido il dataframe in funzione della classe
+            df_classe2 = df[df['Class'] == 2]
+            df_classe4 = df[df['Class'] == 4]
+            # calcolo quale è la numerosita' del test set attesa di ogni classe
+            numerosita_attesa_classe2 = int(round(parametro_splitting*len(df_classe2)))
+            numerosita_attesa_classe4 = int(round(parametro_splitting*len(df_classe4)))
+            # estraggo gli indici delle righe che devono essere nel test set di ogni classe
+            indici_classe_2 = random.sample(range(0, len(df_classe2)), numerosita_attesa_classe2)
             indici_classe_4 = random.sample(range(0, len(df_classe4)), numerosita_attesa_classe4)
-            #creo i test set con le due classi
-            df_classe2_modificato=df_classe2.iloc[indici_classe_2]
-            df_classe4_modificato=df_classe4.iloc[indici_classe_4]
-            testset=pd.concat([df_classe2_modificato,df_classe4_modificato])
-            #creo il train set
+            # creo i test set con le due classi
+            df_classe2_modificato = df_classe2.iloc[indici_classe_2]
+            df_classe4_modificato = df_classe4.iloc[indici_classe_4]
+            testset = pd.concat([df_classe2_modificato, df_classe4_modificato])
+            # creo il train set
             n2 = np.array(range(0, len(df_classe2)))
             n4 = np.array(range(0, len(df_classe4)))
-            #seleziono gli indici che non sono stati usati nel test set
-            indici_train2= list(np.setdiff1d(n2,indici_classe_2))
-            indici_train4= list(np.setdiff1d(n4, indici_classe_4))
+            # seleziono gli indici che non sono stati usati nel test set
+            indici_train2 = list(np.setdiff1d(n2, indici_classe_2))
+            indici_train4 = list(np.setdiff1d(n4, indici_classe_4))
             # identifico gli elementi da includere nel train set
             df_train_classe2_modificato = df_classe2.iloc[indici_train2]
             df_train_classe4_modificato = df_classe4.iloc[indici_train4]
-            trainset=pd.concat([df_train_classe2_modificato, df_train_classe4_modificato])
+            trainset = pd.concat([df_train_classe2_modificato, df_train_classe4_modificato])
             risultato.append((trainset, testset))
         return risultato
-
-
-
-
