cb76e9b0f45f228d99997d551a89251e99ff9733
Mattia Muraro <mattiamuraro8@gmail.com>
2024-01-29 13:03:01 +0100

    è stata ultimato lo splitting con il metodo stratified shuffle subsampling

--- stratifiedShuffleSubsamplingSplitter.py
+++ stratifiedShuffleSubsamplingSplitter.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 from splitting import Splitting
 import pandas as pd
 import random
 class StratifiedShuffleSubsamplingSplitter(Splitting):
-    def split(self, df: pd.DataFrame, ps, n_divisioni, seed=1):
+    def split(self, df: pd.DataFrame, parametro_splitting, n_divisioni, seed=1):
         '''
         Metodo ereditato dalla classe splitting. Serve per splittare con lo stratified shuffle subsampling il dataframe che gli viene passato
         :param df: dataframe da splittare
@@ -12,17 +14,31 @@ class StratifiedShuffleSubsamplingSplitter(Splitting):
         if seed is not None:
             random.seed(seed)
         risultato=[]
-        for i in range(1,n_divisioni):
+        for i in range(0,n_divisioni):
+            #divido il dataframe in funzione della classe
             df_classe2=df[df['Class']==2]
             df_classe4=df[df['Class']==4]
-            numerosita_attesa_classe2=ps*len(df_classe2)
-            numerosita_attesa_classe4=ps*len(df_classe4)
-            indici_classe_2=random.sample(range(1,len(df_classe2)),numerosita_attesa_classe2)
-            indici_classe_4 = random.sample(range(1, len(df_classe4)), numerosita_attesa_classe4)
+            #calcolo quale è la numerosita' del test set attesa di ogni classe
+            numerosita_attesa_classe2=int(round(parametro_splitting*len(df_classe2)))
+            numerosita_attesa_classe4=int(round(parametro_splitting*len(df_classe4)))
+            #estraggo gli indici delle righe che devono essere nel test set di ogni classe
+            indici_classe_2=random.sample(range(0,len(df_classe2)),numerosita_attesa_classe2)
+            indici_classe_4 = random.sample(range(0, len(df_classe4)), numerosita_attesa_classe4)
+            #creo i test set con le due classi
             df_classe2_modificato=df_classe2.iloc[indici_classe_2]
             df_classe4_modificato=df_classe4.iloc[indici_classe_4]
-            risultato.append((df_classe2_modificato,df_classe4_modificato))
-            print(df_classe2,df_classe4)
+            testset=pd.concat([df_classe2_modificato,df_classe4_modificato])
+            #creo il train set
+            n2 = np.array(range(0, len(df_classe2)))
+            n4 = np.array(range(0, len(df_classe4)))
+            #seleziono gli indici che non sono stati usati nel test set
+            indici_train2= list(np.setdiff1d(n2,indici_classe_2))
+            indici_train4= list(np.setdiff1d(n4, indici_classe_4))
+            # identifico gli elementi da includere nel train set
+            df_train_classe2_modificato = df_classe2.iloc[indici_train2]
+            df_train_classe4_modificato = df_classe4.iloc[indici_train4]
+            trainset=pd.concat([df_train_classe2_modificato, df_train_classe4_modificato])
+            risultato.append((trainset, testset))
         return risultato
 
 
