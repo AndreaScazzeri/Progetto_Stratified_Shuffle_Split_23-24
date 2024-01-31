8039621b2db44be2874ac9f69e4edfa6937d690c
Andrea Scazzeri <andrea.scazzeri@alcampus.it>
2024-01-31 12:02:18 +0100

    Si è finalizzato il metodo KNN

--- kNN.py
+++ kNN.py
@@ -9,27 +9,42 @@ class KNN:
     '''
 
     #la funzione doTrain deve solamente calcolare la distanza euclidea di tutti i punti nel train da uno dei punti che stanno nel test
-    def __init__(self,esperimento: list, k=3):
+    def __init__(self,esperimento: list, k=7):
+        """
+        Costruttore della classe kNN:
+        :param esperimento: è una lista che contiene due elementi, il primo è una tupla che contiene le informazioni del
+                            train set, il secondo, anch'esso una tupla che contiene le informazioni del test set.
+        :param k: è il parametro che mi indica quanti valori più vicini al punto devo prendere
+        """
         self.k = k
         self.x_train = esperimento[0][0]
         self.y_train = esperimento[0][1]
         self.x_test = esperimento[1][0]
         self.y_test = esperimento[1][1]
 
-    def doTrain(self, data_train):
-        self.data_train = data_train
-
-    def calculate_distances(self, x):
-        distances = np.linalg.norm(self.data_train - x, axis=1)
-        return distances
+    def calculate_distances(self, row_test):
+        """
+        Questa funzione calcola la distanza tra un elemento del test set e tutti gli elementi del train set restituendo
+        tali distanze rispettivamente agli indici degli elementi del train test
+        """
+        distances = []
+        for index_train, row_train in self.data_train.iterrows():
+            dist = np.linalg.norm(row_train - row_test)
+            distances.append([index_train,dist])
+        return np.array(distances)
 
     def doPrediction(self):
+        """
+        Questa funzione esegue le predizioni su ogni elemento del test e restituisce la predizione rispetto alla classe
+        più numerosa nei k elementi più vicini
+        """
         predictions = []
-        for Data_test in data_test:
-            distances = self.calculate_distances(Data_test)
-            nearest_neighbors_indices = np.argsort(distances)[:self.k]
-            nearest_labels = self.data_train[nearest_neighbors_indices]
+        for index_test, row_test in self.x_test.iterrows():
+            distances = self.calculate_distances(row_test)
+            dist_ordinate = distances[distances[:,1].argsort()]
+            nearest_neighbors_indices = dist_ordinate[:self.k]
+            nearest_labels = self.y_train[nearest_neighbors_indices]
             unique_labels, counts = np.unique(nearest_labels, return_counts=True)
             predicted_label = unique_labels[np.argmax(counts)]
-            predictions.append(predicted_label)
+            predictions.append([index_test,predicted_label])
         return np.array(predictions)
